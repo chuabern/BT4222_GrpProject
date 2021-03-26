8e8883eadcad9d0ea31960b531a03552c23bac00
bchua-pm <bernard.chua@policyme.com>
2021-03-26 09:49:12 -0400

    Latest combined dataset

--- combined_scraped_data.py
+++ combined_scraped_data.py
@@ -7,27 +7,31 @@ dataset = pd.read_csv('Clean_data/Kickstarter.csv').drop('Unnamed: 0', axis=1)
 def combine_multiple_csv(files):
   combined_files = pd.DataFrame()
   for file in files:
-    data = pd.read_csv(file).drop('Unnamed: 0', axis=1)
+    data = pd.read_csv(file)
+    if 'Unnamed: 0' in data.columns:
+      data = data.drop('Unnamed: 0', axis=1)
     combined_files = combined_files.append(data)
+  combined_files['id'] = combined_files['id'].apply(lambda row: int())
   return combined_files
 
 # Add in story text
-story_files = glob.glob("Output/Story/Story*.csv")
+story_files = glob.glob("Output/Story*.csv")
 story = combine_multiple_csv(story_files)
 
-# Add in FAQ/text ? (not sure which one to use here)
-# faq = pd.read_csv('Output/FAQ/FAQ_combined_0to1000.csv').drop('Unnamed: 0', axis=1)
-# faq_text = pd.read_csv('Output/FAQ/FAQ_text_0-100.csv').drop('Unnamed: 0', axis=1)
 
-# Add in updates
-updates_files = glob.glob("Output/Update/Update*.csv")
-updates = combine_multiple_csv(updates_files)
+# Add in FAQ
+faq = glob.glob("Output/FAQ/FAQ_combined_0to3429.csv")
+faq = combine_multiple_csv(faq)
+
+### Not using updates anymore
+# updates_files = glob.glob("Output/Update/Update*.csv")
+# updates = combine_multiple_csv(updates_files)
 
 # Add in comments
 comments_files = glob.glob("Output/Comments/Comment*.csv")
 comments = combine_multiple_csv(comments_files)
 
-data_to_be_combined = [story, updates, comments] # add in FAQ when data has 'id'
+data_to_be_combined = [story, faq, comments] # add in FAQ when data has 'id'
 
 for data in data_to_be_combined:
   dataset = dataset.merge(data, left_on = 'id', right_on = 'id', how = 'left')
