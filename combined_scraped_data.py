23bfbf3ec64b7436d858f6eaaa414b6166f53e44
cocoleah <44935333+cocoleah@users.noreply.github.com>
2021-03-29 10:42:12 +0800

    Update combined_scraped_data.py

--- combined_scraped_data.py
+++ combined_scraped_data.py
@@ -2,8 +2,15 @@ import pandas as pd
 import glob
 from datetime import datetime
 
+# +
 # Use cleaned dataset as the base datatable 
 dataset = pd.read_csv('Clean_data/Kickstarter.csv').drop('Unnamed: 0', axis=1)
+
+dataset.index.name = 'id_row'
+dataset = dataset.reset_index()
+
+
+# -
 
 def combine_multiple_csv(files):
   combined_files = pd.DataFrame()
@@ -24,7 +31,7 @@ story = combine_multiple_csv(story_files)
 faq = glob.glob("Output/FAQ/FAQ_combined_0to3429.csv")
 faq = combine_multiple_csv(faq)
 
-### Not using updates anymore
+# ## Not using updates anymore
 # updates_files = glob.glob("Output/Update/Update*.csv")
 # updates = combine_multiple_csv(updates_files)
 
@@ -34,11 +41,42 @@ comments = combine_multiple_csv(comments_files)
 
 data_to_be_combined = [story, faq, comments] # add in FAQ when data has 'id'
 
-for data in data_to_be_combined:
-  dataset = dataset.merge(data, left_on = 'id', right_on = 'id', how = 'left')
+# +
+# Story
+df1 = data_to_be_combined[0]
+del df1['id']
+df1.index.name = 'id_row'
+df1 = df1.reset_index()
 
-# We only use 1000 data points
-dataset = dataset.iloc[0:1001,]
+# FAQ
+df2 = data_to_be_combined[1]
+del df2['id']
+df2.index.name = 'id_row'
+df2 = df2.reset_index()
+
+# Comments
+df3 = data_to_be_combined[2]
+del df3['id']
+df3.index.name = 'id_row'
+df3 = df3.reset_index()
+# -
+
+# Merge all df1, df2, df3
+from functools import reduce
+data_to_be_combined = reduce(lambda left,right: pd.merge(left,right,on='id_row'), [df1,df2,df3])
+del data_to_be_combined['id_row']
+data_to_be_combined.index.name = 'id_row'
+data_to_be_combined = data_to_be_combined.reset_index()
+data_to_be_combined.tail(1)
+
+dataset = pd.merge(dataset, data_to_be_combined, on='id_row', how='left')
+dataset = dataset.iloc[:1000]
+
 dataset['duration'] = dataset['deadline'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S")) - dataset['launched_at'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S"))
+
+print(dataset.columns)
+dataset
+
+
 
 dataset.to_csv("Output/Combined_dataset.csv")
